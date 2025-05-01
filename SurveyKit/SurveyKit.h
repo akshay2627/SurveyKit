a920cbe9d7cf1aa31d3be73f6f789bd6d811c920
Dev Atini <dev@atinistudio.com>
2025-05-01 14:33:13 +0530

    Initial Commit

---
+++ SurveyKit/SurveyKit.h
@@ -0,0 +1,18 @@
+//
+//  SurveyKit.h
+//  SurveyKit
+//
+//  Created by Akshay Garg on 01/05/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SurveyKit.
+FOUNDATION_EXPORT double SurveyKitVersionNumber;
+
+//! Project version string for SurveyKit.
+FOUNDATION_EXPORT const unsigned char SurveyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SurveyKit/PublicHeader.h>
+
+
---
+++ SurveyKitTests/SurveyKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyKitTests.swift
+//  SurveyKitTests
+//
+//  Created by Akshay Garg on 01/05/25.
+//
+
+import Testing
+@testable import SurveyKit
+
+struct SurveyKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
